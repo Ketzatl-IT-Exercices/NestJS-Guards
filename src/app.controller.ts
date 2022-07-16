a2eb1aad8eb06048584648cd5ac7afdb69835876
Morgan <morgan.nott@hotmail.com>
2022-07-16 14:24:34 +0200

    Modification du Guard : Récupérer dans le guard des meta-données portées par une méthode du controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
+import {Body, Controller, Get, Post, SetMetadata, UseGuards} from '@nestjs/common';
 import { AppService } from './app.service';
 import {EcoloGuard} from "./guards/ecolo.guard";
 
@@ -13,6 +13,7 @@ export class AppController {
 
   @Post('destination')
   @UseGuards(EcoloGuard)
+  @SetMetadata('greenVehicles', ['bike', 'foot', 'horse'])
   travelTo(@Body('vehicle') vehicle: string) {
     console.log("You're inside travelTo method....")
     return `Have a good trip with your ${vehicle}`;
--- src/guards/ecolo.guard.ts
+++ src/guards/ecolo.guard.ts
@@ -1,17 +1,22 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {CanActivate, ExecutionContext, Injectable} from '@nestjs/common';
+import {Observable} from 'rxjs';
+import {Reflector} from "@nestjs/core";
 
 @Injectable()
 export class EcoloGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const req = context.switchToHttp().getRequest();
-    console.log("\n You're Inside Ecologuard....  / body", req.body);
-    if (req.body.vehicle !== 'bike') {
-      return false;
-    } else {
-      return true;
+    constructor(private readonly reflector: Reflector) {
     }
-  }
+
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const greenVehicles = this.reflector.get<string[]>('greenVehicles', context.getHandler())
+        const req = context.switchToHttp().getRequest();
+        console.log("\n You're Inside Ecologuard....  / greenVehicles", greenVehicles);
+        if (req.body.vehicle !== 'bike') {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
